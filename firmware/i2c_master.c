82c159ba3df76e8c78302c9f6939284a16864d3c
g4lvanix <elia@eliaselectronics.com>
2015-08-29 13:39:57 +0200

    added more convenient register access functions as they are very commonly used

--- firmware/i2c_master.c
+++ firmware/i2c_master.c
@@ -1,5 +1,5 @@
 #ifndef  F_CPU
-#define F_CPU 16000000UL
+#define F_CPU 8000000UL
 #endif
 
 #include <avr/io.h>
@@ -95,6 +95,41 @@ uint8_t i2c_transmit(uint8_t address, uint8_t* data, uint16_t length)
 uint8_t i2c_receive(uint8_t address, uint8_t* data, uint16_t length)
 {
 	if (i2c_start(address | 0x01)) return 1;
+
+	for (uint16_t i = 0; i < (length-1); i++)
+	{
+		data[i] = i2c_read_ack();
+	}
+	data[(length-1)] = i2c_read_nack();
+
+	i2c_stop();
+
+	return 0;
+}
+
+uint8_t i2c_writeReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t length)
+{
+	if (i2c_start(devaddr | 0x00)) return 1;
+
+	i2c_write(regaddr);
+
+	for (uint16_t i = 0; i < length; i++)
+	{
+		if (i2c_write(data[i])) return 1;
+	}
+
+	i2c_stop();
+
+	return 0;
+}
+
+uint8_t i2c_readReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t length)
+{
+	if (i2c_start(devaddr)) return 1;
+
+	i2c_write(regaddr);
+
+	if (i2c_start(devaddr | 0x01)) return 1;
 
 	for (uint16_t i = 0; i < (length-1); i++)
 	{
